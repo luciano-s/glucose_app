7196d5dd44d3df3387eb1f6da2dee6ea755b20ca
luciano-s <lcsjr70@gmail.com>
2022-03-27 16:23:31 -0300

    test(use_case): add test for use case

---
+++ glucose_app/api/measurement/tests.py
@@ -0,0 +1,33 @@
+from typing import OrderedDict
+import pytest
+
+from model_bakery import baker
+from datetime import datetime, timezone
+from api.measurement import CreateMeasurementSerializer, CreateMeasurementUseCase
+
+
+@pytest.mark.django_db
+class TestCreateMeasurementUseCase:
+    def test_execute(self, mocker):
+        p = baker.make("pacient.Pacient")
+
+        data = {"glicemy": 100, "pacient": p.id, "date": f"2022-03-27 15:30"}
+        serializer = CreateMeasurementSerializer(data=data)
+
+        assert serializer.is_valid(raise_exception=True)
+        repository = repository = mocker.Mock()
+        use_case = CreateMeasurementUseCase(
+            data=serializer.validated_data, repository=repository
+        )
+
+        use_case.execute()
+        d = OrderedDict(
+            {
+                "date": datetime(
+                    year=2022, month=3, day=27, hour=15, minute=30, tzinfo=timezone.utc
+                ),
+                "glicemy": 100,
+                "pacient": p,
+            }
+        )
+        repository.create_measurement.assert_called_once_with(data=d)
