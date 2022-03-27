53ac81049101f937d456065bd36c4262dec73189
luciano-s <lcsjr70@gmail.com>
2022-03-27 16:24:03 -0300

    refactor(measurement): make glicemy a measurement field

--- glucose_app/measurement/models.py
+++ glucose_app/measurement/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Measurement(models.Model):
 
     timestamp = models.DateTimeField()
-    glicemy = models.ForeignKey("glicemy.Glicemy", null=True, on_delete=models.CASCADE)
+    glicemy = models.IntegerField(null=False, blank=False, default=0)
     pacient = models.ForeignKey("pacient.Pacient", null=True, on_delete=models.CASCADE)
