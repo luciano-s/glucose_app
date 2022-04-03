3a9ace584d10cefeefe5e1d57015ed37c5c4a83a
luciano-s <lcsjr70@gmail.com>
2022-04-03 00:43:04 -0300

    feat(meal): add meal types

--- glucose_app/meal/models.py
+++ glucose_app/meal/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+
 class Meal(models.Model):
 
+    MEAL_TYPES = (
+        ("BREAKFAST", "Café da manhã"),
+        ("LUNCH", "Almoço"),
+        ("SNACK", "Lanche"),
+        ("DINNER", "Jantar"),
+    )
+
     cho = models.IntegerField()
-    measurement = models.ForeignKey("measurement.Measurement", null=True, on_delete=models.SET_NULL)
-    injection = models.ForeignKey("injection.Injection", null=True, on_delete=models.SET_NULL)
+    measurement = models.ForeignKey(
+        "measurement.Measurement", null=True, on_delete=models.SET_NULL
+    )
+    injection = models.ForeignKey(
+        "injection.Injection", null=True, on_delete=models.SET_NULL
+    )
+    type = models.CharField(choices=MEAL_TYPES, max_length=32, null=True)
