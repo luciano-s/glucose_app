68c7bde96dbaee7f8a2b1bd3cbadb34d147ef00b
luciano-s <lcsjr70@gmail.com>
2022-04-03 00:28:37 -0300

    feat(glucose interval): add fields to set optimal glucose level interval

--- glucose_app/measurement/models.py
+++ glucose_app/measurement/models.py
@@ -4,5 +4,14 @@ from django.db import models
 class Measurement(models.Model):
 
     timestamp = models.DateTimeField()
-    glicemy = models.IntegerField(null=False, blank=False, default=0)
+    glycemia = models.IntegerField(null=False, blank=False, default=0)
     pacient = models.ForeignKey("pacient.Pacient", null=True, on_delete=models.CASCADE)
+
+    @property
+    def is_glycemia_good(self) -> bool or None:
+        if None not in [self.pacient.min_glucose_level, self.pacient.max_glucose_level]:
+            return (
+                self.pacient.min_glucose_level
+                <= self.glycemia
+                <= self.pacient.max_glucose_level
+            )
--- glucose_app/pacient/models.py
+++ glucose_app/pacient/models.py
@@ -5,3 +5,5 @@ class Pacient(models.Model):
     user = models.OneToOneField(
         "user.User", on_delete=models.CASCADE, related_name="pacient"
     )
+    min_glucose_level = models.IntegerField(null=True)
+    max_glucose_level = models.IntegerField(null=True)
