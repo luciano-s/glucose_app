dd8694c5933510ae21522839ffb8a3de4f9dfdac
luciano-s <lcsjr70@gmail.com>
2022-04-05 10:13:26 -0300

    feat(measurement table): add sorting to table and use render to color glycemia

--- frontend/src/components/MeasurementTable/MeasurementTable.tsx
+++ frontend/src/components/MeasurementTable/MeasurementTable.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Col, Row, Table } from "antd";
+import moment from "moment";
+import { Col, Table } from "antd";
 import { IMeasurement } from "../../types";
 import { Container } from "../../styled_components/Container";
 import { getColor } from "../../utils";
@@ -13,31 +14,40 @@ export const MeasurementTable: React.FC<IProps> = ({ measurements }) => {
       title: "ID",
       dataIndex: "id",
       key: "id",
+      sorter: (a: { id: number }, b: { id: number }) => a.id - b.id,
     },
     {
       title: "Glicemia",
       dataIndex: "glycemia",
       key: "glycemia",
+      render: (glycemia: number, measurement: IMeasurement) => (
+        <span style={{ color: getColor(measurement) }}>{glycemia}</span>
+      ),
+      sorter: (a: { glycemia: number }, b: { glycemia: number }) =>
+        a.glycemia - b.glycemia,
     },
-    { title: "Data", dataIndex: "timestamp", key: "timestamp" },
+    {
+      title: "Data",
+      dataIndex: "timestamp",
+      key: "timestamp",
+      sorter: (a: IMeasurement, b: IMeasurement) => {
+        const comp =
+          moment(a.timestamp, "DD-MM-YYYY HH:mm").toDate() >
+          moment(b.timestamp, "DD-MM-YYYY HH:mm").toDate();
+        switch (comp) {
+          case true:
+            return 1;
+          default:
+            return 0;
+        }
+      },
+    },
   ];
 
-  const formatedData = measurements.map((measurement) => ({
-    ...measurement,
-    glycemia: (
-      <span
-        style={{
-          color: getColor(measurement),
-        }}
-      >
-        {measurement.glycemia}
-      </span>
-    ),
-  }));
   return (
     <Container>
       <Col span={24}>
-        <Table dataSource={formatedData} columns={columns} />
+        <Table dataSource={measurements} columns={columns} />
       </Col>
     </Container>
   );
