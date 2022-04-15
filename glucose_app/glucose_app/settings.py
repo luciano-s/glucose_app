c9949ef8c762371fb6ae9eb0a7271d0fbd91ab6c
luciano-s <lcsjr70@gmail.com>
2022-04-15 18:45:39 -0300

    feat: set default filter class

--- glucose_app/glucose_app/settings.py
+++ glucose_app/glucose_app/settings.py
@@ -148,5 +148,6 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend"),
 }
