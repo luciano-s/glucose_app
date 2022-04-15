1eee6f4da9e5141d17a08177988ec597505ed673
luciano-s <lcsjr70@gmail.com>
2022-04-15 18:45:01 -0300

    ref: remove field from serializer

--- glucose_app/api/measurement/serializer.py
+++ glucose_app/api/measurement/serializer.py
@@ -16,4 +16,3 @@ class CreateMeasurementSerializer(serializers.Serializer):
     timestamp = serializers.DateTimeField("%Y-%m-%dT%H:%M")
     glycemia = serializers.IntegerField(min_value=0)
     pacient = serializers.PrimaryKeyRelatedField(queryset=Pacient.objects.all())
-    is_glycemia_good = serializers.BooleanField(read_only=True)
