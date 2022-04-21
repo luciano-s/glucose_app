79c3fb501d069e3e8a446724c2d97f8dbee64ca6
luciano-s <lcsjr70@gmail.com>
2022-04-21 17:41:08 -0300

    feat(insulin): add base dosis and link insulin to pacient

--- glucose_app/insulin/models.py
+++ glucose_app/insulin/models.py
@@ -32,6 +32,8 @@ class Insulin(models.Model):
 
     type = models.CharField(choices=INSULIN_TYPES, max_length=50)
     name = models.CharField(max_length=100)
-
+    dosis = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    pacient = models.ForeignKey("pacient.Pacient", on_delete=models.CASCADE, null=True)
+    
     def __str__(self) -> str:
         return f"Insulin {self.type[1]} {self.name}"
